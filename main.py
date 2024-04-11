9799cbc4364ecbc9541882c5c7ba8acc5028cac8
Alexey <74595315+iHandsFromAss@users.noreply.github.com>
2024-04-12 01:37:47 +0500

    Запуск fastapi бэкенда

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.gzip import GZipMiddleware
+
+app = FastAPI()
+app.add_middleware(GZipMiddleware, minimum_size=8000)
+
+app.mount("/css", StaticFiles(directory="templates/css"), name="css")
+app.mount("/js", StaticFiles(directory="templates/js"), name="js")
+app.mount("/img", StaticFiles(directory="templates/img"), name="img")
+app.mount("/fonts", StaticFiles(directory="templates/fonts"), name="fonts")
+app.mount("/Build", StaticFiles(directory="templates/Build"), name="unity_build")
+app.mount("/TemplateData", StaticFiles(directory="templates/TemplateData"), name="unity_template")
+#шаблоны
+templates_templates = Jinja2Templates(directory="templates")
+
+# Рендеринг шаблона index.html из директории "templates"
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates_templates.TemplateResponse("index.html", {"request": request})
+
+# Рендеринг шаблона index.html из директории "templates"
+@app.get("/index.html", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates_templates.TemplateResponse("index.html", {"request": request})
